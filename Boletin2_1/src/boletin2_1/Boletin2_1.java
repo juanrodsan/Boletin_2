134f835b18dbf4e53e6a1634b17d37c9532849e7
jrodriguezsanjorge <jrodriguezsanjorge@nautilus29.danielcastelao.org>
2018-10-09 11:44:08 +0200

    ejercicio 1

---
+++ Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jrodriguezsanjorge
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+    float precio=0,tarifa=0,porcentaje=0;
+    System.out.println("teclea el precio");
+    precio=ler.nextFloat();
+    System.out.println("teclea la tarifa");
+    tarifa=ler.nextFloat();
+    porcentaje=precio-precio*tarifa/100;
+    System.out.println("el porcentaje es"+porcentaje);
+    }
+    
+}
